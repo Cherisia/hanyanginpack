97070fe0e58cc497a65255e8ec4386ae2b6eb84c
Cherisia <yhcyoon@kakao.com>
2024-06-20 17:56:30 +0900

    푸터 작업중 (반응형 해야됨)

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
-import Navbar from "@/components/Navbar";
 import Main from "@/components/Main";
+import Footer from "@/components/Footer";
 
 export default function Home() {
     return (
         <div>
             <Main/>
+            <Footer/>
         </div>
     )
 }
