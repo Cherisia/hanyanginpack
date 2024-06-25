a5fcb6f8c586996340ffdfe4bd6689400e44475a
Cherisia <yhcyoon@kakao.com>
2024-06-25 11:25:31 +0900

    카톡버튼 안눌리는 현상 수정

--- src/components/KakaoChatButton.js
+++ src/components/KakaoChatButton.js
@@ -6,7 +6,7 @@ import Image from "next/image";
 export default function KakaoChatButton() {
     let NEXT_PUBLIC_KAKAO_APP_KEY = process.env.NEXT_PUBLIC_KAKAO_APP_KEY;
     return (
-        <div className="fixed right-6 bottom-6">
+        <div className="fixed right-6 bottom-6 z-30">
             <Script src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js"
                     integrity="sha384-TiCUE00h649CAMonG018J2ujOgDKW/kVWlChEuu4jK2vxfAAD0eZxzCKakxg55G4"
                     crossOrigin="anonymous" onLoad={() => {
