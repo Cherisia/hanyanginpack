c50659cabcd8927f89d95980b9654aa1c86622cd
Cherisia <yhcyoon@kakao.com>
2024-08-01 18:18:58 +0900

    회사 비전 작업중ㅇㅇ

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,11 +1,13 @@
 import Banner from "@/components/about/Banner";
 import MainSection from "@/components/about/MainSection";
+import Visions from "@/components/about/Visions";
 
 export default function About() {
     return (
         <div className="w-full top-[4.5rem] relative bg-gray-50 pb-10">
             <Banner/>
             <MainSection/>
+            <Visions/>
         </div>
     )
 }
