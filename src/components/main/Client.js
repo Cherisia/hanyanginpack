f1ca5503791287c99b8015f7c6ca743559a68712
Cherisia <yhcyoon@kakao.com>
2026-03-03 16:39:08 +0900

    캐러샐 사이즈 조정

--- src/components/main/Client.js
+++ src/components/main/Client.js
@@ -65,7 +65,7 @@ export default function Client() {
                         clients.map((item, index) => {
                             return (
                                 <div className="relative w-1/3 h-24 outline-none" key={index}>
-                                    <Image className="px-8 py-7 lg:px-10 lg:py-6" src={item.img}
+                                    <Image className="object-contain px-10 py-8 lg:px-14 lg:py-8" src={item.img}
                                            alt={item.name} sizes={100} fill/>
                                 </div>
                             )
